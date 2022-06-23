3e95e1231e5c05d328814e18484a7a572cfbee23
Emilie Cote <EmilieCote@MacBook-Pro.local>
2022-06-23 11:37:51 -0700

    rm all py files from main dir

--- __init__.py
+++
@@ -1,4 +0,0 @@
-from .main import conv_h5 as convert
-from .plotting import plot_EOS
-
-print('See ComPOSE (https://compose.obspm.fr/table) and stellarcollapse (https://stellarcollapse.org/equationofstate.html)')
