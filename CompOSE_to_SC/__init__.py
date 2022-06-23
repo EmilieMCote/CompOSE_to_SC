3dfd82ef9224a1f979de0ef1bd987f0b633e709b
tedjohnson12 <76502099+tedjohnson12@users.noreply.github.com>
2022-06-23 15:23:03 -0700

    fix typo in message

--- CompOSE_to_SC/__init__.py
+++ CompOSE_to_SC/__init__.py
@@ -3,4 +3,4 @@ from CompOSE_to_SC.plotting import plot_EOS
 
 __version__='1.0.0'
 
-print('See ComPOSE (https://compose.obspm.fr/table) and stellarcollapse (https://stellarcollapse.org/equationofstate.html)')
+print('See CompOSE (https://compose.obspm.fr/table) and stellarcollapse (https://stellarcollapse.org/equationofstate.html)')
