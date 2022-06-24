f4bd5834ae4875febe15b59c54c078d50a6a3b47
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-06-24 16:05:23 -0700

    changed default download location

--- CompOSE_to_SC/download.py
+++ CompOSE_to_SC/download.py
@@ -20,15 +20,15 @@ def untar(path):
     file.close
     return head+ '/' +tail.split('.')[0] + '.h5'
 
-def download(url,untar_file = True):
+def download(url,new_path = './',untar_file = True):
     filename = url.split('/')[-1]
-    new_path = '../PracticeFiles/'+filename
-    response = wget.download(url,new_path)
+    path = new_path+filename
+    response = wget.download(url,path)
     if untar_file:
         if filename.split('.')[-1] == 'bz2':
-            system('bzip2 -d %s' % new_path)
-            return new_path.split('.')[0] + '.h5'
+            system('bzip2 -d %s' % path)
+            return path.split('.')[0] + '.h5'
         else:
-            return untar(filename)
+            return untar(path)
     else:
-        return filename
+        return path
