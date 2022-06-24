9bd58cafa3bd3032f6aac445d6e4117fe446476d
Ted Johnson <tedjohnson12@g.ucla.edu>
2022-06-24 13:35:38 -0700

    add length 1 list test case

--- tests/test_plotting.py
+++ tests/test_plotting.py
@@ -4,24 +4,62 @@ from os import system
 import h5py
 from matplotlib.figure import Figure
 
-def test_plotting_blank():
-    """Test plotting a 'blank' H5 file
+def make_blank(filename):
+    """Create blank H5 file
+
+    Make a blank file to use in tests. Make sure to remove these files afterwards.
+
+    Args:
+        filename (str): string containing path or filename of H5 file containing all zeros.
+        This file will be in the StellarCollapse format
+    
+    Returns:
+        None
+    """
+    infile = 'blank.h5'
+    with blank as h5py.File(infile,'r'):
+        convert(infile, outfile)
+    # now remove blank
+    system('rm %s' % infile)
+
+def test_plotting_single_blank(show = False):
+    """Test plotting from a 'blank' H5 file
 
     Create a blank H5 file of all zeros and plot it
 
     Args:
-        None
+        show (bool): whether to plot the figure
 
     Returns:
         None
     
     """
-    # create blank file
-    infile = 'blank.h5'
-    outfile = 'zeros.h5'
-    with blank as h5py.File(infile,'r'):
-        convert(infile, outfile)
-        fig = plot_EOS(infile)
-        assert isinstance(fig,Figure)
-    # now remove blank and zeros
-    system('rm %s %s' % (infile,outfile))
+    # create zero file
+    converted_file = 'zeros.h5'
+    make_blank(converted_file)
+    fig = plot_EOS(converted_file)
+    assert isinstance(fig,Figure)
+    if show:
+        fig.show()
+
+def test_plotting_len1_list_blank(show = False):
+    """Test plotting from a 'blank' H5 file
+
+    Create a blank H5 file of all zeros and plot it as a list
+
+    Args:
+        show (bool): whether to plot the figure
+
+    Returns:
+        None
+    
+    """
+    # create zero file
+    converted_file = 'zeros.h5'
+    make_blank(converted_file)
+    fig = plot_EOS([converted_file])
+    assert isinstance(fig,Figure)
+    if show:
+        fig.show()
+
+    
